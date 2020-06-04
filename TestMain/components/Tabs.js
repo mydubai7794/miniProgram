c00974278407e91eabec4f0565d60a39d4ab684d
L-xuanda <380554538@qq.com>
2020-06-04 18:16:42 +0800

    second

---
+++ TestMain/components/Tabs.js
@@ -0,0 +1,29 @@
+// components/Tabs.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {},
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    tabs: [
+      {
+        id: 0,
+        name: "推荐",
+        isActive: false,
+      },
+      {
+        id: 1,
+        name: "发现",
+        isActive: true,
+      },
+    ],
+  },
+
+  methods: {
+    handleintap() {},
+  },
+});
---
+++ TestMain/pages/fresh/index.js
@@ -0,0 +1,49 @@
+const innerAudioContext = wx.createInnerAudioContext();
+Page({
+  onLoad: function (e) {
+    this.data.index = 0;
+    // 使用 wx.createAudioContext 获取 audio 上下文 context
+    // this.audioCtx = wx.createAudioContext("myAudio");
+  },
+  data: {
+    index: 0,
+  },
+
+  handlemusic: function (e) {
+    console.log(e);
+
+    if (0 == this.data.index) {
+      console.log(0);
+
+      innerAudioContext.play();
+      this.setData({
+        index: 1,
+      });
+    } else {
+      console.log(1);
+
+      innerAudioContext.pause();
+      this.setData({
+        index: 0,
+      });
+    }
+  },
+  handlenav: function (e) {
+    wx.navigateTo({
+      url: "../fresh-info1/index",
+    });
+  },
+  onShow: function () {
+    innerAudioContext.src =
+      "http://music.163.com/song/media/outer/url?id=562598065.mp3";
+    //音频的数据链接，用于直接播放。支持云文件ID（2.2.3起）。
+  },
+  //   //点击播放,(如果要一进来就播放放到onload即可)
+  //   handlemusic: function () {
+  //     innerAudioContext.play();
+  //   },
+  //   //点击 停止
+  //   stop: function () {
+  //     innerAudioContext.pause();
+  //   },
+});
