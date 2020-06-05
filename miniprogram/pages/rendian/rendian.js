34303b79b7f779f5ef201f78fed515d9380e16c7
gityuyu-111 <2601059312@qq.com>
2020-06-05 15:14:58 +0800

    yy/2020/6/5

--- miniprogram/pages/rendian/rendian.js
+++ miniprogram/pages/rendian/rendian.js
@@ -5,7 +5,7 @@ const innerAudioContext = wx.createInnerAudioContext();
     // 使用 wx.createAudioContext 获取 audio 上下文 context 
      // this.audioCtx = wx.createAudioContext("myAudio"); 
    }, 
-   data: { index:0,
+   data: { 
   }, 
   handlemusic: function (e) { 
      console.log(e); 
@@ -29,7 +29,6 @@ const innerAudioContext = wx.createInnerAudioContext();
    onShow: function () { 
      innerAudioContext.src = 
        "http://music.163.com/song/media/outer/url?id=1337883508.mp3"; 
-       "http://music.163.com/song/media/outer/url?id=25640795.mp3";
      //音频的数据链接，用于直接播放。支持云文件ID（2.2.3起）。 
    }, 
    //   //点击播放,(如果要一进来就播放放到onload即可) 
